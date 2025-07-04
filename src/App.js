40be430e345f5208e584c1832735b7b84be431b2
Dharmistha Thakor <tdharmistha1417@gmail.com>
2025-07-04 18:04:49 +0530

    create screen

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import './App.css';
 import Navbar from './components/Navbar';
+import Create from "./components/Create";
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
         <Navbar />
-        {/* <Routes>
-          <Route exact path="/" element={<Read />} />
-          <Route exact path="/create" element={<Create />} />
-          <Route exact path="/edit/:id" element={<Edit />} />
-        </Routes> */}
+        {/* <Create /> */}
+        <Routes>
+          {/* <Route exact path="/" element={<Read />} /> */}
+          <Route exact path="/" element={<Create />} />
+          {/* <Route exact path="/edit/:id" element={<Edit />} /> */}
+        </Routes>
       </BrowserRouter>
     </div>
   );
--- src/components/Create.jsx
+++ src/components/Create.jsx
@@ -1,1 +1,40 @@
-rafc
+import react, { useState } from "react";
+
+const Create = () => {
+
+    const [users,setUsers] = useState({});
+    setUsers({...users ,[e.target.name] : e.target.value })
+    {name : "himanshu"}
+    return (
+        <div >
+
+            <form className="w-50 mx-auto my-5">
+                <div class="mb-3">
+                    <label class="form-label">Name</label>
+                    <input type="email" name="name" class="form-control" />
+                </div>
+                <div class="mb-3">
+                    <label class="form-label">Email</label>
+                    <input type="email" name="email" class="form-control" />
+                </div>
+                <div class="mb-3">
+                    <label class="form-label">Age</label>
+                    <input type="age" name="age" class="form-control" />
+                </div>
+                <div class="mb-3 ">
+                    <label class="form-check-label me-3" >Male</label>
+                    <input type="checkbox" name="gender" value="Male" class="form-check-input" />
+                </div>
+                  <div class="mb-3">
+                    <label class="form-check-label me-3" >Female</label>
+                    <input type="checkbox" name="gender" value="Female" class="form-check-input" />
+                </div>
+             
+                <button type="submit" class="btn btn-primary">Submit</button>
+            </form>
+        </div>
+
+    )
+}
+
+export default Create
